ee105d88a785e74a8db40321c509914aa6434108
Diego Valenzuela <diegovalenzuela85@gmail.com>
2021-05-21 11:46:32 -0500

    Ejemplos semana 3

--- semana_3_for.py
+++ semana_3_for.py
@@ -55,3 +55,99 @@
 #
 # print()
 # print('Final')
+
+
+# EJEMPLOS FOR PDF -------------------------------------------------------------------------------
+
+# print('Comienzo')
+# for i in [0,1,2]:
+#     print(f'Hola {i}')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for i in []:
+#     print(f'Hola')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for i in [1,1,1]:
+#     print(f'Hola {i}')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for _ in [0,1,2]:
+#     print(f'Hola')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for i in [3,4,5]:
+#     print(f'Hola. Ahora i vale {i} y su cuadrado es {i**2}')
+# print('Final')
+
+
+# print('Comienzo')
+# for i in ["Alba","Benito",27]:
+#     print(f'Hola. Ahora i vale {i}')
+# print('Final')
+
+
+# print('Comienzo')
+# for numero in [0,1,2,3]:
+#     print(f'{numero} * {numero} = {numero**2}')
+# print('Final')
+
+# i = 10
+# print(f'El bucle no ha comenzado. Ahora i vale {i}')
+# for i in [0,1,2,3,4]:
+#     print(f'{i} * {i} = {i**2}')
+# print(f'El bucle ha terminado. Ahora i vale {i}')
+
+
+# for i in "AMIGO":
+#     print(f'Dame una {i}')
+# print('¡AMIGO!')
+
+
+# print('Comienzo')
+# for numero in range(3):
+#     print(f'Hola ',end='')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for i in range(10):
+#     print(f'Hola ',end='')
+# print()
+# print('Final')
+
+
+# print('Comienzo')
+# for i in range(5):
+#     print(f'Hola ',end='')
+# print()
+# print('Adiós')
+
+
+# for i in [0,1,2]:
+#     for j in [0,1]:
+#         print(f'i vale {i} y j vale {j}')
+
+
+# for i in range(3):
+#     print(f'i (externa) vale {i}')
+#     for i in range(2):
+#         print(f'i (interna) vale {i}')
+
+
+# for i in [1,2,3]:
+#     for j in range(i):
+#         print(f'i vale {i} y j vale {j}')

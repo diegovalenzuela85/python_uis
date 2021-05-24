694440ac7cc00ee7e9f01ac4f6f133d41428968a
diegovalenzuela85 <49688183+diegovalenzuela85@users.noreply.github.com>
2021-05-24 00:05:57 -0500

    ciclo reto 3 menu

--- reto_semana_3.py
+++ reto_semana_3.py
@@ -81,36 +81,55 @@
 #
 # Solucione el reto, escribiendo el código que represente la automatización del proceso de registro de empleados y visitantes.
 
-ingreso_usuario = ""
-ingreso_contrasena = ""
+# ingreso_usuario = ""
+# ingreso_contrasena = ""
+#
+# usuario = 'admin'
+# contrasena = 'MisionTic2021'
+#
+# intentos = 0
+#
+# while usuario != ingreso_usuario or contrasena != ingreso_contrasena and intentos < 3:
+#     print('Datos incorrectos. Por favor intenta nuevamente')
+#     ingreso_usuario = input('usuario: ')
+#     ingreso_contrasena = input('contraseña: ')
+#     intentos += 1
+#
+#     if usuario == ingreso_usuario and contrasena == ingreso_contrasena:
+#         seleccion = int(input('\n1. Registrar ingreso de empleado.\n2. Ver empleados ingresados.\n'
+#               '3. Registrar ingreso de visitantes.\n4. Ver visitantes ingresados.\n'
+#               '0. Salir.\n-> '))
+#         if seleccion == 1:
+#             print('estas en 1')
+#         if seleccion == 2:
+#             print('estas en 2')
+#         if seleccion == 3:
+#             print('estas en 3')
+#         if seleccion == 4:
+#             print('estas en 4')
+#         if seleccion == 0:
+#             print('Saliste.. muchas gracias')
+#
+# if usuario != ingreso_usuario and contrasena != ingreso_contrasena:
+#     print('\nAdios,no pudiste ingresar.')
 
-usuario = 'admin'
-contrasena = 'MisionTic2021'
-
-intentos = 0
-
-while usuario != ingreso_usuario or contrasena != ingreso_contrasena and intentos < 3:
-    print('Datos incorrectos. Por favor intenta nuevamente')
-    ingreso_usuario = input('usuario: ')
-    ingreso_contrasena = input('contraseña: ')
-    intentos += 1
-
-    if usuario == ingreso_usuario and contrasena == ingreso_contrasena:
-        seleccion = int(input('\n1. Registrar ingreso de empleado.\n2. Ver empleados ingresados.\n'
-              '3. Registrar ingreso de visitantes.\n4. Ver visitantes ingresados.\n'
-              '0. Salir.\n-> '))
-        if seleccion == 1:
-            print('estas en 1')
-        if seleccion == 2:
-            print('estas en 2')
-        if seleccion == 3:
-            print('estas en 3')
-        if seleccion == 4:
-            print('estas en 4')
-        if seleccion == 0:
-            print('Saliste.. muchas gracias')
-
-if usuario != ingreso_usuario and contrasena != ingreso_contrasena:
-    print('\nAdios,no pudiste ingresar.')
-
-
+while True:
+    seleccion = int(input('''
+    [1] Registrar ingreso de empleado.
+    [2] Ver empleados ingresados.
+    [3] Registrar ingreso de visitantes.
+    [4] Ver visitantes ingresados.
+    [0] Salir.
+    Escribe la acción que deseas realizar: 
+    '''))
+    if seleccion == 1:
+        print('estas en 1')
+    elif seleccion == 2:
+        print('estas en 2')
+    elif seleccion == 3:
+        print('estas en 3')
+    elif seleccion == 4:
+        print('estas en 4')
+    elif seleccion == 0:
+        print('Saliste.. muchas gracias')
+        break
